0aa0d9aa791656d27965e7184fe5e31bbd74a95e
sakshi <sakshi@gmail.com>
2026-01-12 15:56:26 +0530

    Added User Verification Code Into CI/CD

--- terraform-gcp/modules/compute_instance/outputs.tf
+++ terraform-gcp/modules/compute_instance/outputs.tf
@@ -3,5 +3,5 @@ output "vm_name" {
 }
 
 output "vm_ip" {
-  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.packer_vm.network_interface[0].access_config[0].nat_ip
 }
